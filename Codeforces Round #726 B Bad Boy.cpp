aeef6a2430ce83f7a9de744b41964de1ae7010b2
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-09-05 23:21:58 +0530

    bad boy

---
+++ Codeforces Round #726 B Bad Boy.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t > 0)
+    {
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+        cout << "1 1 " << n << " " << m << endl;
+        t--;
+    }
+}
